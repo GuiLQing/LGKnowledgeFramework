a494ffbbf808aaa967a79b83758c99ac1e9e5a9b
PSG <gui950823@126.com>
2019-06-12 09:30:51 +0800

    Initial commit

---
+++ Example/LGKnowledgeFramework/LGViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGViewController.h
+//  LGKnowledgeFramework
+//
+//  Created by GuiLQing on 06/12/2019.
+//  Copyright (c) 2019 GuiLQing. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LGViewController : UIViewController
+
+@end
---
+++ Example/LGKnowledgeFramework/LGViewController.m
@@ -0,0 +1,29 @@
+//
+//  LGViewController.m
+//  LGKnowledgeFramework
+//
+//  Created by GuiLQing on 06/12/2019.
+//  Copyright (c) 2019 GuiLQing. All rights reserved.
+//
+
+#import "LGViewController.h"
+
+@interface LGViewController ()
+
+@end
+
+@implementation LGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
